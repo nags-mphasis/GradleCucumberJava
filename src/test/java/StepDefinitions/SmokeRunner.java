2a6e2a15f9eef86e6d12422ca218020d797cb8d4
nags-mphasis <nagarajan.ganapathy@mphasis.com>
2021-03-15 13:52:08 +0530

    Runner file modified

--- src/test/java/StepDefinitions/SmokeRunner.java
+++ src/test/java/StepDefinitions/SmokeRunner.java
@@ -5,19 +5,11 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-// plugin = {"pretty", "json:target/JSONReports/Reports.json"}
-// plugin = {"pretty", "html:target/HTMLReports/Reports.html"}
-// plugin = {"pretty", "junit:target/JunitReports/Reports.xml"}
-
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features", glue={"StepDefinitions"},
 	monochrome = true,
-	plugin = {"pretty", "json:target/smokeReports/Reports.json"
-	//		"pretty", "html:target/HTMLReports/Reports.html",
-	//		"pretty", "junit:target/JunitReports/Reports.xml"
-	},
-	tags = "@SmokeTest"
-	)
+	plugin = {"pretty", "json:target/smokeReports/Reports.json"},
+	tags = "@SmokeTest")
 public class SmokeRunner {
 
 }
